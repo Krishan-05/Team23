dbcc39f12adbf3ef45ba762a97f1d75af5cab9de
Christian Endale <230035665@aston.ac.uk>
2024-10-23 11:22:03 +0100

    added min function, changed file name

---
+++ MathClass.java
@@ -0,0 +1,11 @@
+public class MathClass {
+    
+  /********************************
+    Developer: Christian Endale
+    University ID: 230035665
+    Function: This function takes two inputs as integers and returns the minimum value
+    ********************************/
+    int min(int x, int y) {
+        return Math.min(x, y);
+    }  
+}
