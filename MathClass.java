f41ef505d63be09484f07b7008a0ae98028ab4b4
Krishan-05 <krish17@outlook.com>
2024-10-29 23:36:53 +0000

    Added Multiplication Function

--- MathClass.java
+++ MathClass.java
@@ -1,6 +1,11 @@
-//CREATE YOUR OWN BRANCH AND GIT PULL BEFORE!!!
-public class MathClass{
+public class MathClass {
     
-}
-
-    
+    /********************************
+      Developer: Krishan Patel
+      University ID: 230112955
+      Function: This function takes two inputs as integers and returns the multiplication of both numbers
+      ********************************/
+      int min(int x, int y) {
+          return x * y;
+      }  
+  }
