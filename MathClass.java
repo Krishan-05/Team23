79f9b7f15ff87c2664ecb8d20646e3816f8c25d4
Maida Nadeem <230066692@aston.ac.uk>
2024-10-30 09:08:10 +0000

    Add Function

--- MathClass.java
+++ MathClass.java
@@ -1,5 +1,14 @@
 //CREATE YOUR OWN BRANCH AND GIT PULL BEFORE!!!
 public class MathClass{
+    /******************
+     Developer: Maida Nadeem
+     University ID: 230066692
+     Function: Add function, which takes two inputs as integers and returns the sum
+    *********************/
+
+   int add(int x, int y){
+      return x+y
+   }
     
 }
 
