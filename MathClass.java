05902c4a4e75a57906c9ee735685ac0481b3cfaf
Benson Daniel <230184125@aston.ac.uk>
2024-10-30 09:23:13 +0000

    Math class, Divide function added

--- MathClass.java
+++ MathClass.java
@@ -2,28 +2,25 @@
 
 import java.util.Scanner;
 
-public class MathClass{
-    public class DivisionCalculator {
-        public static void main(String[] args) {
-            Scanner Scanner  = new Scanner(System.in);
-            System.out.print("Enter the numerator: ");
-            double numerator = Scanner.nextDouble();
-            System.out.print("Enter the numerator: ");
-            double denominator = Scanner.nextDouble();
-
-            if (denominator ==0){
-                System.out.println("erroe cannot divide by zero");
-            }else{
-                System.out.println("result: " + (numerator / denominator));
-            }
-            Scanner.close();
-            }
-
-            
-        }
-    
+public class MathClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Enter the numerator: ");
+        double numerator = scanner.nextDouble();
+        
+        System.out.print("Enter the denominator: "); 
+        double denominator = scanner.nextDouble();
+
+        if (denominator == 0) {
+            System.out.println("Error: cannot divide by zero");
+        } else {
+            System.out.println("Result: " + (numerator / denominator));
+        }
+        
+        scanner.close(); 
     }
+}
     
 
     
