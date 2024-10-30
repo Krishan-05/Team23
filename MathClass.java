ccff17001b153cab812064675f0110842416331c
Bisma64 <230068478@aston.ac.uk>
2024-10-30 09:18:36 +0000

    Subtract Function

--- MathClass.java
+++ MathClass.java
@@ -1,5 +1,16 @@
 //CREATE YOUR OWN BRANCH AND GIT PULL BEFORE!!!
 public class MathClass{
+    /******************************** 
+    Developer: Bisma Babar 
+    University ID: 230068478
+    Function: This function takes two integers as input, subtract the second 
+    input from first one and returns the result. 
+    ********************************/ 
+    int Sub(int x, int y) 
+    { 
+       return y-x;
+} 
+
     
 }
 
