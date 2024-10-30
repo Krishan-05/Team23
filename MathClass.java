9e8027627d17d469082106a1a90f7c2f8097d6fb
Benson Daniel <230184125@aston.ac.uk>
2024-10-30 09:07:09 +0000

    test1

--- MathClass.java
+++ MathClass.java
@@ -1,6 +1,29 @@
 //CREATE YOUR OWN BRANCH AND GIT PULL BEFORE!!!
+
+import java.util.Scanner;
+
 public class MathClass{
+    public class DivisionCalculator {
+        public static void main(String[] args) {
+            Scanner Scanner  = new Scanner(System.in);
+            System.out.print("Enter the numerator: ");
+            double numerator = Scanner.nextDouble();
+            System.out.print("Enter the numerator: ");
+            double denominator = Scanner.nextDouble();
+
+            if (denominator ==0){
+                System.out.println("erroe cannot divide by zero");
+            }else{
+                System.out.println("result: " + (numerator / denominator));
+            }
+            Scanner.close();
+            }
+
+            
+        }
     
-}
+        
+    }
+    
 
     
