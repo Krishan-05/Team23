a4df6c8445d82d59079d9876a1d7a289a1d120af
Krishan-05 <krish17@outlook.com>
2024-11-11 23:28:37 +0000

    Pulled files from KrishanBranch. (Homepage Sevelopment Start)

--- MathClass.java
+++ MathClass.java
@@ -1,11 +1,4 @@
 public class MathClass {
     
-    /********************************
-      Developer: Krishan Patel
-      University ID: 230112955
-      Function: This function takes two inputs as integers and returns the multiplication of both numbers
-      ********************************/
-      int min(int x, int y) {
-          return x * y;
-      }  
+
   }
